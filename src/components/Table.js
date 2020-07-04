4496fc179a84aad53f30ffb22aa92257276ed23f
Mudit <muditw@amazon.com>
2020-07-05 00:04:50 +0530

    :)

--- src/components/Table.js
+++ src/components/Table.js
@@ -34,7 +34,7 @@ function Table(props) {
                                     {
                                         props.data[index].map((innerValue, innerIndex) => {
                                             if(innerIndex === 0) {
-                                                return <th scope="row">{innerValue}</th>
+                                                return <th scope="row" className="col-sm-6 col-md-3">{innerValue}</th>
                                             } else {
                                                 return <td className="col-sm-6 col-md-3">{innerValue}</td>
                                                 /*return <td className="col-sm-6 col-md-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</td>*/
