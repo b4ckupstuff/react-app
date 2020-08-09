70c7186ba25830799a805d6845ac6038f1207c1a
Mudit <muditw@amazon.com>
2020-08-09 11:23:49 +0530

    :)

---
+++ src/components/componentCss/ResultCards.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+function ResultCards(props) {
+    return (
+        <div>
+            <div className="row row-cols-1 row-cols-md-2">
+                <div className="col mb-4">
+                    <div className="card border-dark mb-3" style="max-width: 18rem;">
+                        <div className="card-header">Header</div>
+                        <div className="card-body text-dark">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card border-dark mb-3" style="max-width: 18rem;">
+                        <div className="card-header">Header</div>
+                        <div className="card-body text-dark">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card border-dark mb-3" style="max-width: 18rem;">
+                        <div className="card-header">Header</div>
+                        <div className="card-body text-dark">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card border-dark mb-3" style="max-width: 18rem;">
+                        <div className="card-header">Header</div>
+                        <div className="card-body text-dark">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResultCards;
--- src/containers/ResultContainer.js
+++ src/containers/ResultContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import Breadcrumb from "../components/componentCss/Breadcrumb";
 import ThreeXThreeGrid from "../components/ThreeXThreeGrid";
+import ResultCards from "../components/componentCss/ResultCards";
 
 function ResultContainer(props) {
     const history = useHistory();
@@ -14,6 +15,8 @@ function ResultContainer(props) {
                     levelOne="Home"
                     levelTwo="Your Name Here"
                 />
+
+                <ResultCards />
 
                 <ThreeXThreeGrid />
 
